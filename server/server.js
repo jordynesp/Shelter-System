5456abbfb5cb76f62ac2f901ac551a9d78f449a8
jordynesp <93738138+jordynesp@users.noreply.github.com>
2021-11-22 17:27:38 -0600

    Created initial files used for web server.
    Created necessary files needed to set up our web server within a docker container.

---
+++ server/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Load packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mysql = require("mysql");
+const port = 8080;
+
+// Connect to the mysql databse
+const connection = mysql.createConnection({
+    host: 'mysql',
+    port: '3306',
+    user: 'root',
+    password: 'admin'
+});
+connection.connect(err => {
+    if (err) throw err;
+    connection.query("USE shelter;", (err, result) => {
+        if (err) throw err;
+        console.log("Connected to database!");
+    })
+});
+
+// Parses incoming request bodies
+app.use(bodyParser.urlencoded({extended:true}));
+
+// Set up routing
+app.use("/", express.static("/app/src/pages"));
+app.get("/", (req, res) => {
+    res.redirect("/home.html");
+});
+
+// Make the app listen on port 8080
+app.listen(port, function() {
+ console.log("Server listening on http://localhost:" + port);
+});
